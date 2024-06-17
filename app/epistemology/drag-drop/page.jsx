9754b088bb14fb04fba7eb5d197250cb94bf4516
Sagaust <austinorge@yahoo.com>
2024-06-17 14:24:37 +0000

    fixiiing

---
+++ app/epistemology/drag-drop/page.jsx
@@ -0,0 +1,14 @@
+// app/epistemology/drag-drop/page.jsx
+import React from "react";
+import PhilosopherGame from "../../../components/PhilosopherGame";
+
+const DragDropPage = () => {
+  return (
+    <div>
+      <h1>Drag and Drop Philosophers</h1>
+      <PhilosopherGame />
+    </div>
+  );
+};
+
+export default DragDropPage;
